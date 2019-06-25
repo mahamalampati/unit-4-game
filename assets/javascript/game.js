c4b1a60ce4b753fbe2673b339cd2c2bbc0fa7a03
maha <mahashevata@gmail.com>
2019-06-25 15:44:18 -0500

    need more updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-var number =[18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120]
+var number =[18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120];
   
 var computerNumber = Math.floor(Math.random()*103);
 
@@ -9,8 +9,8 @@ $("#number-to-guess").text(targetNumber);
 var counter = 0;
 
 
-var numberOptions = [1,2,3,4,5,6,7,8,9,10,11,12];
-var computerOptions= Math.floor(Math.random()*13);
+var numberOptions1 = [1,2,3,4,5,6,7,8,9,10,11,12];
+var computerOptions1= Math.floor(Math.random()*13);
 
 
 
@@ -19,120 +19,91 @@ var computerOptions= Math.floor(Math.random()*13);
   var imageCrystal1 = $("<img>");
 
  
-  imageCrystal1.addClass("crystal-image1");
+  imageCrystal1.addClass("crystal-image");
 
   
   imageCrystal1.attr("src", "assets/images/image 1.jpg");
 
  
-  imageCrystal1.attr("data-crystalvalue1",numberOptions[computerOptions]);
+  imageCrystal1.attr("data-crystalvalue",numberOptions1[computerOptions1]);
    
 
   
   $("#crystals").append(imageCrystal1);
 
+  var numberOptions2 = [1,2,3,4,5,6,7,8,9,10,11,12];
+  var computerOptions2= Math.floor(Math.random()*13);
+
   var imageCrystal2 = $("<img>");
 
   
-  imageCrystal2.addClass("crystal-image2");
+  imageCrystal2.addClass("crystal-image");
 
   
   imageCrystal2.attr("src", "assets/images/image 2.jpg");
 
   
-  imageCrystal2.attr("data-crystalvalue2",numberOptions[computerOptions]);
+  imageCrystal2.attr("data-crystalvalue",numberOptions2[computerOptions2]);
    
 
   
   $("#crystals").append(imageCrystal2);
 
-
+  var numberOptions3 = [1,2,3,4,5,6,7,8,9,10,11,12];
+  var computerOptions3= Math.floor(Math.random()*13);
   var imageCrystal3 = $("<img>");
 
 
-  imageCrystal3.addClass("crystal-image3");
+  imageCrystal3.addClass("crystal-image");
 
   
   imageCrystal3.attr("src", "assets/images/image 3.jpg");
 
 
-  imageCrystal3.attr("data-crystalvalue3",numberOptions[computerOptions]);
+  imageCrystal3.attr("data-crystalvalue",numberOptions3[computerOptions3]);
    
 
   
   $("#crystals").append(imageCrystal3);
 
-
+  var numberOptions4 = [1,2,3,4,5,6,7,8,9,10,11,12];
+  var computerOptions4= Math.floor(Math.random()*13);
   var imageCrystal4 = $("<img>");
 
  
-  imageCrystal4.addClass("crystal-image4");
+  imageCrystal4.addClass("crystal-image");
 
   
   imageCrystal4.attr("src", "assets/images/image 4.jpg");
 
  
-  imageCrystal4.attr("data-crystalvalue4",numberOptions[computerOptions]);
+  imageCrystal4.attr("data-crystalvalue",numberOptions4[computerOptions4]);
    
 
   
   $("#crystals").append(imageCrystal4);
 
-$(".crystal-image1").on("click", function() {
-
- 
-
-  var crystalValue1 = ($(this).attr("data-crystalvalue1"));
-  crystalValue1 = parseInt(crystalValue1);
-
-  counter += crystalValue1;
 
   
-  alert("New score: " + counter);});
 
-  $(".crystal-image2").on("click", function() {
+  $(".crystal-image").on("click", function() {
 
- 
 
-    var crystalValue2 = ($(this).attr("data-crystalvalue2"));
-    crystalValue2 = parseInt(crystalValue2);
+
+    var crystalValue = ($(this).attr("data-crystalvalue"));
+    crystalValue = parseInt(crystalValue);
   
-    counter += crystalValue2;
-  
-    
-    alert("New score: " + counter);});
+    counter += crystalValue;
 
-    $(".crystal-image3").on("click", function() {
+   
+    alert("New score: " + counter);
 
- 
+    if (counter === targetNumber) {
+      alert("You win!");
+    }
 
-      var crystalValue3 = ($(this).attr("data-crystalvalue3"));
-      crystalValue3 = parseInt(crystalValue3);
-    
-      counter += crystalValue3;
-    
-      
-      alert("New score: " + counter);
+    else if (counter >= targetNumber) {
+      alert("You lose!!");
+    }
 
-      $(".crystal-image4").on("click", function() {
-
- 
-
-        var crystalValue4 = ($(this).attr("data-crystalvalue4"));
-        crystalValue4 = parseInt(crystalValue4);
-      
-        counter += crystalValue4;
-      
-        
-        alert("New score: " + counter)});
-
-  if (counter === targetNumber) {
-    alert("You win!");
-  }
-
-  else if (counter >= targetNumber) {
-    alert("You lose!!");
-  }
-
-
-    })
+  });
