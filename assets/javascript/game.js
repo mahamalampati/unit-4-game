6a6e98c94f9a7330fe89e19b1e806f24136b753e
maha <mahashevata@gmail.com>
2019-06-26 11:37:01 -0500

    logic done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,31 @@
-var number =[18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120];
+var number;
   
-var computerNumber = Math.floor(Math.random()*103);
+var computerNumber;
 
-var targetNumber = number[computerNumber];
+var targetNumber;
 
 $("#number-to-guess").text(targetNumber);
 
-var counter = 0;
+var counter ;
 var winCount = 0;
 var lossCount = 0;
+
+var reset = function(){
+
+  counter = 0;
+   number =[18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120];
+  
+   computerNumber = Math.floor(Math.random()*103);
+  
+  targetNumber = number[computerNumber];
+  
+  $("#number-to-guess").text(targetNumber);
+
+  $("#total-count").text(counter);
+  
+  
+  
+}
 
  
 var numberOptions1 = [1,2,3,4,5,6,7,8,9,10,11,12];
@@ -97,7 +114,7 @@ var computerOptions1= Math.floor(Math.random()*13);
   
     counter += crystalValue;
 
-    $("#total-count").text("New score: " + counter);
+    $("#total-count").text(counter);
     
 
     if (counter === targetNumber) {
@@ -109,16 +126,32 @@ var computerOptions1= Math.floor(Math.random()*13);
     
     $("#win-count").text(winCount);
 
+    reset();
+
     }
   
 
     else if (counter >= targetNumber) {
+
+      reset();
       alert("You lose!!");
       
 
       lossCount++;
 
-    // Change Loss Count HTML
+    
     $("#loss-count").text(lossCount);
+    reset();
+    
+    
     }
   });
+
+  
+  window.onload = function()
+
+  {
+       reset();
+    
+
+  }
